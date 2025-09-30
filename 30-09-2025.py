12850e89c566b47e153c413614140d45cd474279
nitesh jangra <niteshjan06@gmail.com>
2025-09-30 16:34:26 +0000

    nothing

--- 30-09-2025.py
+++ 30-09-2025.py
@@ -1,3 +1,13 @@
 """
-what is  the name of ':' in python ?
----> 
+to convert a integer number into binary than change its 2nd lsb to 1 and then again convert it to integer value.
+"""
+
+# main code
+
+number = int(input("Enter any number: "))
+binary = bin(number)
+binary_list = list(str(binary))
+binary_list[-2] = '1'
+str="".join(binary_list)
+
+print(int(str,2))
