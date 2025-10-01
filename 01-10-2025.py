f204fb33b02c5abd6ef37e6dd09b2d3c2684a9c7
niteshjan06 <niteshjan06@gmail.com>
2025-10-01 21:03:47 +0530

    Update 01-10-2025.py

--- 01-10-2025.py
+++ 01-10-2025.py
@@ -1,1 +1,14 @@
+"""
+---> what is the difference between normal statements and executable statements ?
+"""
+
+#answer
+"""
+1. Normal Statements
+---> these are general-purpose statements in a programming language.They may or may not perform an actual execution step.       example -- import statements(import math),declaration(int n)
+
+2.Executable statements
+---> these are the statements that are actually executed at runtime.They perform some operation or action(computation,input/output,assignment,function,call,control flow,etc.)      example -- loops,conditionals,function calls
+
+"""
 # nothing to say
